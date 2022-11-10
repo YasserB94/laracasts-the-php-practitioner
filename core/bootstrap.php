c61170096d6c0a84ad6689857a31c6e33818f362
YasserB94 <yasserbal@icloud.com>
2022-11-10 17:36:27 +0100

    Added MVC routing with 2 routes

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -7,7 +7,7 @@ require 'core/Request.php';
 
 require 'core/db/DbConnection.php';
 require 'core/db/DbQueryBuilder.php';
+$app['database'] = new DbQueryBuilder(DbConnection::make($app['config']['database']));
 
 require "models/Task.php";
-$app['database'] = new DbQueryBuilder(DbConnection::make($app['config']['database']));
 
---
+++ views/about.view.php
@@ -0,0 +1,8 @@
+<?php
+require 'partials/head.php';
+?>
+<h1>Hello About!</h1>
+<?php
+require 'partials/footer.php'
+?>
+
